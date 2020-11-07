5d113d5a0a9644f83a64779c670fe7f993a2ac83
MukheshRavi <mukeshparvathi@gmail.com>
2020-11-07 23:09:37 +0530

    [Mukhesh].Add Hotels added in Hotel Reservation System

---
+++ Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+   public class Hotel
+    {
+        public string hotelName;
+        public int rate;
+        public List<Hotel> hotelsList = new List<Hotel>();
+        public Hotel() { }
+        public Hotel(string name)
+        {
+            this.hotelName = name;
+            if(name=="Lakewood") 
+                rate = 110;
+            if (name == "Bridgewood")
+                rate = 160;
+            if (name == "Ridgewood")
+                rate = 220;
+        }
+        public void AddHotel()
+        {
+            hotelsList.Add(new Hotel("Lakewood"));
+            hotelsList.Add(new Hotel("Bridgewood"));
+            hotelsList.Add(new Hotel("Ridgewood"));
+        }
+        public void DisplayHotels()
+        {
+            AddHotel();
+            Console.WriteLine("HotelName  Rate");
+            foreach(Hotel hotel in hotelsList)
+            {
+                Console.WriteLine(hotel.hotelName + " " + hotel.rate);
+            }
+
+        }
+
+    }
+}
